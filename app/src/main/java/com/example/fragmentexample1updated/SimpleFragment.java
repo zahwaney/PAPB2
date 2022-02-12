e0f6f321724cb2e9cfc747f014edaa79d36b483c
Zahwa Neysa <zahwa.neysa@gmail.com>
2022-02-13 00:38:56 +0700

    first commit

---
+++ app/src/main/java/com/example/fragmentexample1updated/SimpleFragment.java
@@ -0,0 +1,94 @@
+package com.example.fragmentexample1updated;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SimpleFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SimpleFragment extends Fragment {
+
+    private static final int YES = 0;
+    private static final int NO = 1;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public SimpleFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SimpleFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SimpleFragment newInstance(String param1, String param2) {
+        SimpleFragment fragment = new SimpleFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_simple, container, false);
+        RadioGroup radioGroup = view.findViewById(R.id.radio_group);
+        TextView articleQuestionTextview = view.findViewById(R.id.question_textview);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton btn = radioGroup.findViewById(i);
+                int selectedIndex = radioGroup.indexOfChild(btn);
+
+                switch (selectedIndex) {
+                    case YES :
+                        articleQuestionTextview.setText(R.string.yes_message);
+                        break;
+                    case NO :
+                        articleQuestionTextview.setText(R.string.no_message);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
+        // Inflate the layout for this fragment
+        return view;
+    }
+}
